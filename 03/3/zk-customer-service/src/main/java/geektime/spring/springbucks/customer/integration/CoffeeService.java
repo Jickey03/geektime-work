74dd6a1b75a37472975768b1c2f3836141e8b770
jickey <yujilin03@163.com>
2022-12-07 21:22:30 +0800

    极客作业

---
+++ 03/3/zk-customer-service/src/main/java/geektime/spring/springbucks/customer/integration/CoffeeService.java
@@ -0,0 +1,24 @@
+package geektime.spring.springbucks.customer.integration;
+
+import geektime.spring.springbucks.customer.model.Coffee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Component
+@FeignClient(name = "waiter-service", contextId = "coffee", path = "/coffee")
+// 不要在接口上加@RequestMapping
+public interface CoffeeService {
+    @GetMapping(path = "/", params = "!name")
+    List<Coffee> getAll();
+
+    @GetMapping("/{id}")
+    Coffee getById(@PathVariable Long id);
+
+    @GetMapping(path = "/", params = "name")
+    Coffee getByName(@RequestParam String name);
+}
